99b8e572480869c37a0b59cd7f14ffdaa2636501
Zied <ziedtriki013@gmail.com>
2021-01-20 20:15:20 +0100

    fn-design

--- resources/views/star/add.blade.php
+++ resources/views/star/add.blade.php
@@ -37,10 +37,6 @@
 
                         <div class="grid grid-cols-1 gap-6">
                             <div class="col-span-1 sm:col-span-2">
-                                <label for=""
-                                       class="block text-sm font-medium text-gray-700">
-                                    Nom
-                                </label>
                                 <div class="mt-1 flex rounded-md shadow-sm">
                                     <input type="text" name="nom" id="company_website"
                                            class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
@@ -51,10 +47,6 @@
 
                         <div class="grid grid-cols-1 gap-6">
                             <div class="col-span-3 sm:col-span-2">
-                                <label for=""
-                                       class="block text-sm font-medium text-gray-700">
-                                    Prénom
-                                </label>
                                 <div class="mt-1 flex rounded-md shadow-sm">
                                     <input type="text" name="prenom" id="company_website"
                                            class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
@@ -65,9 +57,6 @@
 
                         <div class="grid grid-cols-1 gap-6">
                             <div class="col-span-3 sm:col-span-2">
-                                <label for="" class="block text-sm font-medium text-gray-700">
-                                    Description
-                                </label>
                                 <div class="mt-1">
                                             <textarea id="about" name="description" rows="3"
                                                       class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
@@ -77,29 +66,14 @@
                         </div>
 
 
-                        <div class="grid grid-cols-3 gap-6">
-                            <div class="col-span-1 sm:col-span-2">
-                                <label class="block text-sm font-medium text-gray-700">
-                                    Photo
-                                </label>
-                                <div class="mt-2 flex items-center">
-                <span class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                  <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                        d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"/>
-                  </svg>
-                </span>
-                                </div>
-                            </div>
-
-
-                            <div class="col-span-1 sm:col-span-2">
+                        <div class="grid grid-cols-3 gap-y-4">
+                            <div class="col-span-2 sm:col-span-2">
                                 <input type="file" name="url_image">
                             </div>
                         </div>
 
-                        <div class="grid grid-cols-6 gap-6">
-                            <div class="col-start-2 col-span-4 sm:col-span-2">
+                        <div class="grid grid-cols-8 gap-6">
+                            <div class="col-start-4 col-span-3">
                                 <input type="submit"
                                        class="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 w-44 rounded-full "
                                        value="Valider"/>
